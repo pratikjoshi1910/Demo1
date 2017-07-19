23caa6d6bd978f87cb7ec2ed6f8cb15e2e8ccc35
pratikjoshi1910 <pratikjoshi1910@gmail.com>
2017-07-19 00:11:56 -0700

    test

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo1Application {
 
 	public static void main(String[] args) {
-	hjkknjn
+	
 		SpringApplication.run(pratik.class, args);
 
 
