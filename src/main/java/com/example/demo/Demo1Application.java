c8ecef69258a1c97311001bc48b4d8fe8b41494c
pratikjoshi1910 <pratikjoshi1910@gmail.com>
2017-07-19 00:25:49 -0700

    Update Demo1Application.java

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -9,7 +9,7 @@ public class Demo1Application {
 	public static void main(String[] args) {
 	
 		SpringApplication.run(pratik.class, args);
-	hjkkn
+	
 		SpringApplication.run(pratik.class, arg);
 
 
