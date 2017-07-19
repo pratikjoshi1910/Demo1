8db9599702abae200a5e3a77dcbe8a3aa0e52898
pratikjoshi1910 <pratikjoshi1910@gmail.com>
2017-07-19 00:39:32 -0700

    Update Demo1Application.java

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo1Application {
 
 	public static void main(String[] args) {
-	kal
+	k
 		SpringApplication.run(prrdhjfhghjfdhdh.class, args);
 	
 		SpringApplication.run(pratik.class, arg);
