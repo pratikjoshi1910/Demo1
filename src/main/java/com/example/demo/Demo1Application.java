7ce6b17dfba883b193fa07443e84ec6310ff7de6
pratikjoshi1910 <pratikjoshi1910@gmail.com>
2017-07-19 00:33:29 -0700

    test

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -8,7 +8,7 @@ public class Demo1Application {
 
 	public static void main(String[] args) {
 	
-		SpringApplication.run(prrdhjfhghjfdhdhg.class, args);
+		SpringApplication.run(prrdh.class, args);
 	
 		SpringApplication.run(pratik.class, arg);
 
