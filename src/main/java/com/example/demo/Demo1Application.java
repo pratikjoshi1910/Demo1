2e4a9ae40432da8db9e9d15a15fc3a61267032db
pratikjoshi1910 <pratikjoshi1910@gmail.com>
2017-07-19 04:15:28 -0700

    addding deleted files

---
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1Application {
+
+	public static void main(String[] args) {
+
+
+	
+		SpringApplication.run(Demo1Application.class, args);
+    System.out.println("est");
+
+		
+	}
+	
+	Test test = new Test(); 
+	
+	 public void test()
+	 {
+		 test.test();
+	 }
+	
+	
+}
